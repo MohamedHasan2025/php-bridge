8be1e0d69fdc27625683750fc1e1153ee404debe
MohamedHasan2025 <Mohamed.ashraff@daw.ae>
2025-12-29 15:42:21 +0400

    Update index.php

--- public/index.php
+++ public/index.php
@@ -1,27 +1,11 @@
 <?php
 
-// Path to the system directory
-$systemPath = realpath(__DIR__ . '/../vendor/codeigniter4/framework/system');
-if (!is_dir($systemPath)) {
-    die('System folder not found. Did you run composer install?');
-}
-
-// Path to the application directory
-$applicationPath = realpath(__DIR__ . '/../app');
-
-// Path to writable directory
-$writablePath = realpath(__DIR__ . '/../writable'); 
-
-// Define FCPATH
 define('FCPATH', __DIR__ . DIRECTORY_SEPARATOR);
 
-// Load Composer autoloader
-require FCPATH . '/../vendor/autoload.php';
+require FCPATH . '../vendor/autoload.php';
 
 require FCPATH . '../app/Config/Paths.php';
 
-// Load CodeIgniter
-$paths = new \Config\Paths();
-$app = \Config\Services::request(); // framework bootstrapped automatically
+$paths = new Config\Paths();
+
 require rtrim($paths->systemDirectory, '\\/ ') . DIRECTORY_SEPARATOR . 'bootstrap.php';
-$app->run();
